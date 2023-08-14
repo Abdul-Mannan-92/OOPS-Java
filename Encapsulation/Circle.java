d220557e663c1a2f5f2dce4471a61a58665103c3
Mohammed Abdul Mannan <mohammedabdulmannan2611@gmail.com>
2023-08-13 22:33:33 -0500

    Encapsulation

---
+++ Encapsulation/Circle.java
@@ -0,0 +1,11 @@
+class Circle { 
+    private double radius = 1; 
+    public double getArea() {
+     return radius * radius * Math.PI;
+   }
+   public static void main(String[] args) {
+    Circle myCircle = new Circle();
+    System.out.println("Radius is " + myCircle.radius);
+    System.out.println("Area of cirle: " +myCircle.getArea()); 
+   }
+  }
